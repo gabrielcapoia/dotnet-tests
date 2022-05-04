ac01717412b6e8c49d2e0cf1d719a944e1d307da
Gabriel Capoia <gcapoia@gmail.com>
2022-05-04 15:29:44 -0300

    created a test as simple as possible

---
+++ SoftwareTests/UnitTest/Demo.Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Calculator_Sum_ReturnSumValue()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Sum(5, 10);
+
+            //Assert
+            Assert.Equal(15, result);
+        }
+    }
+}
