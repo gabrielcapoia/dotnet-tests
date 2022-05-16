c7da9df92080590e095d825a155512c1ffdca9ca
Gabriel Capoia <gcapoia@gmail.com>
2022-05-16 17:25:38 -0300

    feature: fluent assertation

---
+++ SoftwareTests/UnitTest/Features.Tests/07 - FluentAssertions/CustomerFluentAssertionsTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Features.Tests
+{
+    [Collection(nameof(CustomerAutoMockerCollection))]
+    public class CustomerFluentAssertionsTests
+    {
+        private readonly CustomerTestsAutoMockerFixture _customerTestsFixture;
+        readonly ITestOutputHelper _outputHelper;
+
+        public CustomerFluentAssertionsTests(CustomerTestsAutoMockerFixture customerTestsFixture, 
+                                            ITestOutputHelper outputHelper)
+        {
+            _customerTestsFixture = customerTestsFixture;
+            _outputHelper = outputHelper;
+        }
+
+
+        [Fact(DisplayName = "New Valid Customer")]
+        [Trait("Category", "Customer Fluent Assertion Tests")]
+        public void Customer_NewCustomer_ShouldBeValid()        
+        {
+            // Arrange
+            var customer = _customerTestsFixture.GenerateValidCustomer();
+
+            // Act
+            var result = customer.IsValid();
+
+            // Assert 
+            //Assert.True(result);
+            //Assert.Equal(0, customer.ValidationResult.Errors.Count);
+
+            // Assert 
+            result.Should().BeTrue();
+            customer.ValidationResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = "New Invalid Customer")]
+        [Trait("Category", "Customer Fluent Assertion Tests")]
+        public void Customer_NewCustomer_ShouldBeInvalid()        
+        {
+            // Arrange
+            var customer = _customerTestsFixture.GenerateInvalidCustomer();
+
+            // Act
+            var result = customer.IsValid();
+
+            // Assert 
+            //Assert.False(result);
+            //Assert.NotEqual(0, customer.ValidationResult.Errors.Count);
+
+            // Assert 
+            result.Should().BeFalse();
+            customer.ValidationResult.Errors.Should().HaveCountGreaterOrEqualTo(1, "must have validation errors");
+
+            _outputHelper.WriteLine($"{customer.ValidationResult.Errors.Count} errors were found in this validation");
+        }
+    }
+}
---
+++ SoftwareTests/UnitTest/Features.Tests/07 - FluentAssertions/CustomerServiceFluentAssertionTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Threading;
+using Features.Customers;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(CustomerAutoMockerCollection))]
+    public class CustomerServiceFluentAssertionTests
+    {
+        readonly CustomerTestsAutoMockerFixture _customerTestsAutoMockerFixture;
+
+        private readonly CustomerService _customerService;
+
+        public CustomerServiceFluentAssertionTests(CustomerTestsAutoMockerFixture customerTestsFixture)
+        {
+            _customerTestsAutoMockerFixture = customerTestsFixture;
+            _customerService = _customerTestsAutoMockerFixture.GetCustomerService();
+        }
+
+        [Fact(DisplayName = "Add Customer with Successful")]
+        [Trait("Category", "Customer Service Fluent Assertion Tests")]
+        public void CustomerService_Add_ShouldRunWithSuccess()        
+        {
+            // Arrange
+            var customer = _customerTestsAutoMockerFixture.GenerateValidCustomer();
+
+            // Act
+            _customerService.Add(customer);
+
+            // Assert
+            //Assert.True(customer.EhValido());
+
+            // Assert
+            customer.IsValid().Should().BeTrue();
+
+            _customerTestsAutoMockerFixture.Mocker.GetMock<ICustomerRepository>().Verify(r => r.Add(customer),Times.Once);
+            _customerTestsAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m=>m.Publish(It.IsAny<INotification>(),CancellationToken.None),Times.Once);
+        }
+
+        [Fact(DisplayName = "Add Customer with Error")]
+        [Trait("Category", "Customer Service Fluent Assertion Tests")]
+        public void CustomerService_Add_ShouldRunWithInvalidCustomerError()        
+        {
+            // Arrange
+            var customer = _customerTestsAutoMockerFixture.GenerateInvalidCustomer();
+
+            // Act
+            _customerService.Add(customer);
+
+            // Assert
+            Assert.False(customer.IsValid());
+
+            // Assert
+            customer.IsValid().Should().BeFalse("There are inconsistencies");
+            customer.ValidationResult.Errors.Should().HaveCountGreaterOrEqualTo(1);
+
+            _customerTestsAutoMockerFixture.Mocker.GetMock<ICustomerRepository>().Verify(r => r.Add(customer), Times.Never);
+            _customerTestsAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Get Actives Customers")]
+        [Trait("Category", "Customer Service Fluent Assertion Tests")]
+        public void CustomerService_GetAllActives_ShouldRetunrOnlyActivesCustomers()        
+        {
+            // Arrange
+            _customerTestsAutoMockerFixture.Mocker.GetMock<ICustomerRepository>().Setup(c => c.GetAll())
+                .Returns(_customerTestsAutoMockerFixture.GetRandomCustomers());
+
+            // Act
+            var customers = _customerService.GetAllActives();
+
+            // Assert 
+            //Assert.True(customers.Any());
+            //Assert.False(customers.Count(c => !c.Ativo) > 0);
+
+            // Assert
+            customers.Should().HaveCountGreaterOrEqualTo(1).And.OnlyHaveUniqueItems();
+            customers.Should().NotContain(c => !c.Active);
+
+            _customerTestsAutoMockerFixture.Mocker.GetMock<ICustomerRepository>().Verify(r => r.GetAll(), Times.Once);
+
+            _customerService.ExecutionTimeOf(c=>c.GetAllActives())
+                .Should()
+                .BeLessOrEqualTo(50.Milliseconds(),
+                    "runs thousands of times per second");
+        }
+    }
+}
