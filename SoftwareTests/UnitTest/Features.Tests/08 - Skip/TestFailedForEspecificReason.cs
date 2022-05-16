ea07573094cbf6bd01322bf4185ecc92f0512f8b
Gabriel Capoia <gcapoia@gmail.com>
2022-05-16 17:34:56 -0300

    feature: refactoring

---
+++ SoftwareTests/UnitTest/Features.Tests/08 - Skip/TestFailedForEspecificReason.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    public class TestFailedForEspecificReason
+    {
+        [Fact(DisplayName = "New Customer 2.0", Skip = "New version 2.0 breaking")]
+        [Trait("Category", "Skiping tests")]
+        public void Test_NotPassing_NewVersionBreaking()
+        {
+            Assert.True(false);
+        }
+    }
+}
--- SoftwareTests/UnitTest/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
+++ SoftwareTests/UnitTest/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
@@ -6,14 +6,14 @@ namespace Features.Tests
 {
     public class CpfValidationTests
     {
-        [Theory(DisplayName = "CPF Validos")]
-        [Trait("Categoria", "CPF Validation Tests")]
+        [Theory(DisplayName = "Valids CPF")]
+        [Trait("Category", "CPF Validation Tests")]
         [InlineData("15231766607")]
         [InlineData("78246847333")]
         [InlineData("64184957307")]
         [InlineData("21681764423")]
         [InlineData("13830803800")]
-        public void Cpf_ValidarMultiplosNumeros_TodosDevemSerValidos(string cpf)
+        public void Cpf_ValidateManyNumbers_AllShouldBeValids(string cpf)
         {
             // Assert
             var cpfValidation = new CpfValidation();
@@ -22,15 +22,15 @@ namespace Features.Tests
             cpfValidation.IsValid(cpf).Should().BeTrue();
         }
 
-        [Theory(DisplayName = "CPF Invalidos")]
-        [Trait("Categoria", "CPF Validation Tests")]
+        [Theory(DisplayName = "Invalids CPF")]
+        [Trait("Category", "CPF Validation Tests")]
         [InlineData("15231766607213")]
         [InlineData("528781682082")]
         [InlineData("35555868512")]
         [InlineData("36014132822")]
         [InlineData("72186126500")]
         [InlineData("23775274811")]
-        public void Cpf_ValidarMultiplosNumeros_TodosDevemSerInvalidos(string cpf)
+        public void Cpf_ValidateManyNumbers_AllShouldBeInvalids(string cpf)
         {
             // Assert
             var cpfValidation = new CpfValidation();
