cc1d86cea86abfc586b35e614d43aac773716dcf
Gabriel Capoia <gcapoia@gmail.com>
2022-06-06 16:14:12 -0300

    tests: config fixture

---
+++ SoftwareTests/IntegrationTest/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests.Config
+{ 
+
+    [CollectionDefinition(nameof(IntegrationWebTestFixtureCollection))]
+    public class IntegrationWebTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>>
+    {
+
+    }
+
+    [CollectionDefinition(nameof(IntegrationApiTestFixtureCollection))]
+    public class IntegrationApiTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>>
+    {
+
+    }
+
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly AppStoreFactory<TStartup> Factory;
+        public HttpClient Client;
+
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new AppStoreFactory<TStartup>();
+            Client = Factory.CreateClient(clientOptions);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ SoftwareTests/IntegrationTest/tests/NerdStore.WebApp.Tests/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UserTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> testsFixture;
+
+        public UserTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+        {
+            this.testsFixture = testsFixture;
+        }
+    }
+}
