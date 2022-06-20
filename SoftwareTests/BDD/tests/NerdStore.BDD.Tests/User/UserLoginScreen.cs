8f1b65101dde33914c12b85ca560b677dbed0a81
Gabriel Capoia <gcapoia@gmail.com>
2022-06-20 15:45:41 -0300

    test: BDD user login tests

---
+++ SoftwareTests/BDD/tests/NerdStore.BDD.Tests/User/UserLoginScreen.cs
@@ -0,0 +1,53 @@
+﻿using NerdStore.BDD.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.BDD.Tests.User
+{
+    public class UserLoginScreen : BaseUserScreen
+    {
+        public UserLoginScreen(SeleniumHelper helper) : base(helper)
+        {
+        }
+
+        public void ClicarNoLinkLogin()
+        {
+            Helper.ClicarLinkPorTexto("Login");
+        }
+
+        public void PreencherFormularioLogin(Usuario usuario)
+        {
+            Helper.PreencherTextBoxPorId("Input_Email", usuario.Email);
+            Helper.PreencherTextBoxPorId("Input_Password", usuario.Senha);
+        }
+
+        public bool ValidarPreenchimentoFormularioLogin(Usuario usuario)
+        {
+            if (Helper.ObterValorTextBoxPorId("Input_Email") != usuario.Email) return false;
+            if (Helper.ObterValorTextBoxPorId("Input_Password") != usuario.Senha) return false;
+
+            return true;
+        }
+
+        public void ClicarNoBotaoLogin()
+        {   
+            var botao = Helper.ObterElementoPorXPath("//*[@id='login-submit']");
+            botao.Click();
+        }
+
+        public bool Login(Usuario usuario)
+        {
+            AcessarSiteLoja();
+            ClicarNoLinkLogin();
+            PreencherFormularioLogin(usuario);
+            if (!ValidarPreenchimentoFormularioLogin(usuario)) return false;
+            ClicarNoBotaoLogin();
+            if (!ValidarSaudacaoUsuarioLogado(usuario)) return false;
+
+            return true;
+        }
+    }
+}
--- SoftwareTests/BDD/tests/NerdStore.BDD.Tests/User/User_LoginSteps.cs
+++ SoftwareTests/BDD/tests/NerdStore.BDD.Tests/User/User_LoginSteps.cs
@@ -1,27 +1,56 @@
-﻿using System;
+﻿using NerdStore.BDD.Tests.Config;
+using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace NerdStore.BDD.Tests.User
 {
     [Binding]
+    [CollectionDefinition(nameof(AutomacaoWebFixtureCollection))]
     public class User_LoginSteps
     {
+        private readonly UserLoginScreen _userLoginScreen;
+        private readonly AutomacaoWebTestsFixture _testsFixture;
+
+        public User_LoginSteps(AutomacaoWebTestsFixture testsFixture)
+        {
+            _testsFixture = testsFixture;
+            _userLoginScreen = new UserLoginScreen(_testsFixture.BrowserHelper);
+        }
+
         [When(@"he clicks login")]
         public void WhenHeClicksLogin()
         {
-            ScenarioContext.Current.Pending();
+            // Act
+            _userLoginScreen.ClicarNoLinkLogin();
+
+            // Assert
+            Assert.Contains(_testsFixture.Configuration.LoginUrl,
+                _userLoginScreen.ObterUrl());
         }
         
         [When(@"fill in the login form data")]
         public void WhenFillInTheLoginFormData(Table table)
         {
-            ScenarioContext.Current.Pending();
+            // Arrange
+            var usuario = new Usuario
+            {
+                Email = "teste1@teste.com",
+                Senha = "Teste@123"
+            };
+            _testsFixture.Usuario = usuario;
+
+            // Act
+            _userLoginScreen.PreencherFormularioLogin(usuario);
+
+            // Assert
+            Assert.True(_userLoginScreen.ValidarPreenchimentoFormularioLogin(usuario));
         }
         
         [When(@"click the login button")]
         public void WhenClickTheLoginButton()
         {
-            ScenarioContext.Current.Pending();
+            _userLoginScreen.ClicarNoBotaoLogin();
         }
     }
 }
